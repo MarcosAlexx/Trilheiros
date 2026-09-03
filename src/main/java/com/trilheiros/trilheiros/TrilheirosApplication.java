f3b416aa54bda0f2ba62eded60ff9d23463e614a
MarcosAlexx <marcosentic@gmail.com>
2026-09-03 20:28:51 -0300

    Initial Spring Boot project scaffold

---
+++ src/main/java/com/trilheiros/trilheiros/TrilheirosApplication.java
@@ -0,0 +1,13 @@
+package com.trilheiros.trilheiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilheirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilheirosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilheiros/trilheiros/TrilheirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilheiros.trilheiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilheirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
