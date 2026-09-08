3a66e3d81a78f9e6cb18756bce2fb41b4c9857b9
MarcosAlexx <marcosentic@gmail.com>
2026-09-07 23:02:11 -0300

    Add Flyway migrations for trail, user_trail and Trailreview tables

--- src/main/resources/db/migration/V3__create_trail_table.sql
+++ src/main/resources/db/migration/V3__create_trail_table.sql
@@ -4,10 +4,9 @@ CREATE TABLE trails (
                           description VARCHAR(255) NOT NULL,
                           city VARCHAR(100) NOT NULL,
                           state VARCHAR(100) NOT NULL,
-                          location VARCHAR(100) NOT NULL,
                           difficulty VARCHAR(50) NOT NULL,
-                          distanceKm DECIMAL(5,2) NOT NULL,
-                          durationHours DECIMAL(5,2) NOT NULL,
+                          distance_km DECIMAL(5,2) NOT NULL,
+                          duration_hours DECIMAL(5,2) NOT NULL,
                           longitude DECIMAL(10,7) NOT NULL,
                           latitude DECIMAL(10,7) NOT NULL,
                           contact VARCHAR(100) NOT NULL
