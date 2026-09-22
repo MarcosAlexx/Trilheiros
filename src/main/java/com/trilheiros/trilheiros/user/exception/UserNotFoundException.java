36e2fb556e61363f495f2722c9da89754d79d1c0
MarcosAlexx <marcosentic@gmail.com>
2026-09-21 22:42:13 -0300

    New builder

--- src/main/java/com/trilheiros/trilheiros/user/exception/UserNotFoundException.java
+++ src/main/java/com/trilheiros/trilheiros/user/exception/UserNotFoundException.java
@@ -4,4 +4,8 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String email) {
         super("User not found: " + email);
     }
+
+    public UserNotFoundException(Long id) {
+        super("User not found: " + id);
+    }
 }
