fa4fddd15910f7526d35a8cc0a9ed88507bd8099
MarcosAlexx <marcosentic@gmail.com>
2026-09-22 00:18:52 -0300

    Add TrailReview response DTO

--- src/main/java/com/trilheiros/trilheiros/trail/dto/TrailReviewResponseDTO.java
+++ src/main/java/com/trilheiros/trilheiros/trail/dto/TrailReviewResponseDTO.java
@@ -1,4 +1,13 @@
 package com.trilheiros.trilheiros.trail.dto;
 
-public record TrailReviewResponseDTO() {
+import java.time.LocalDateTime;
+
+public record TrailReviewResponseDTO(
+    Long id,
+    Long trailId,
+    Long userId,
+    int rating,
+    String comment,
+    LocalDateTime createdAt
+) {
 }
