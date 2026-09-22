43a0686e00cdbbd327753bda6d02b2c1efaff96e
MarcosAlexx <marcosentic@gmail.com>
2026-09-22 00:19:02 -0300

    Add findByTrailId to TrailReviewRepository

--- src/main/java/com/trilheiros/trilheiros/trail/repository/TrailReviewRepository.java
+++ src/main/java/com/trilheiros/trilheiros/trail/repository/TrailReviewRepository.java
@@ -2,8 +2,8 @@ package com.trilheiros.trilheiros.trail.repository;
 
 import com.trilheiros.trilheiros.trail.entity.TrailReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-import java.util.Optional;
 
 public interface TrailReviewRepository extends JpaRepository<TrailReview, Long> {
 
